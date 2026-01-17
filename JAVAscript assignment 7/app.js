c61f60a76ae91ee199ef49b1841871e612083594
mirajghani05 <mirajghani05@gmail.com>
2026-01-17 22:51:11 +0500

    Refactor app.js for better variable usage

--- JAVAscript assignment 7/app.js
+++ JAVAscript assignment 7/app.js
@@ -13,11 +13,11 @@ console.log("Round of value " + Math.round(num2));
 console.log("floor value "+ Math.floor(num2));
 console.log("ceil value"+ Math.ceil(num2));
 // chapter 3 assignmenmt 7
-let num1 = -4;
-let num2 = 5;
+var num3= -4;
+var num4 = 5;
 
-document.write("Absolute value of " + num1 + " is " + Math.abs(num1) + "<br>");
-document.write("Absolute value of " + num2 + " is " + Math.abs(num2) + "<br>");
+document.write("Absolute value of " + num3 + " is " + Math.abs(num1) + "<br>");
+document.write("Absolute value of " + num4 + " is " + Math.abs(num2) + "<br>");
 // ch 4
 // Dice values range from 1 to 6
 let dice = Math.floor(Math.random() * 6) + 1;
@@ -36,4 +36,5 @@ document.write("Random number between 1 and 100: " + randomNum);
 // Chap 7
 let userInput = prompt("Enter your weight:");
 let weight = parseFloat(userInput); // Extracts numeric value
+
 document.write("Your weight is: " + weight + " kilograms");
